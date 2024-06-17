c4801376893f7b337ab406eebe29cf630359b485
Nicolas Juan Ignacio Torres <nicoignacio123@gmail.com>
2024-06-16 23:56:50 -0300

    fix: agregando las validaciones de las clases en el service y corrigiendo los resources

--- BackendSofftek/obra-social-api/src/main/java/resource/AffiliateResource.java
+++ BackendSofftek/obra-social-api/src/main/java/resource/AffiliateResource.java
@@ -12,7 +12,6 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import service.interfaces.IAffiliateService;
-import validator.AffiliateValidator;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,40 +24,29 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 @Tag(name = "Afiliados")
 public class AffiliateResource {
 
-	private IAffiliateService affiliateService;
-	private AffiliateValidator affiliateValidator;
-	
 	@Inject
-	public AffiliateResource(IAffiliateService affiliateService, AffiliateValidator affiliateValidator) {
-		super();
-		this.affiliateService = affiliateService;
-		this.affiliateValidator = affiliateValidator;
-	}
+	private IAffiliateService affiliateService;
 
 	@GET
 	@Operation(summary = "Obtener todos los afiliados", description = "Retorna una lista de todos los afiliados de la obra social.")
-	@APIResponses(value = {
-			@APIResponse(responseCode = "200", description = "Afiliados obtenidos con éxito"),
-			@APIResponse(responseCode = "204", description = "No hay afiliados cargados en el sistema")
-	})
+	@APIResponses(value = { @APIResponse(responseCode = "200", description = "Afiliados obtenidos con éxito"),
+			@APIResponse(responseCode = "204", description = "No hay afiliados cargados en el sistema") })
 	public Response getAllAffiliates() {
 		List<Affiliate> affiliates = affiliateService.getAllAffiliates();
-		if(!affiliates.isEmpty()) {
-		List<AffiliateDTO> affiliateDTOs = affiliates.stream().map(AffiliateMapper::entityToDto)
-				.collect(Collectors.toList());
-		return Response.ok(affiliateDTOs).build();
+		if (!affiliates.isEmpty()) {
+			List<AffiliateDTO> affiliateDTOs = affiliates.stream().map(AffiliateMapper::entityToDto)
+					.collect(Collectors.toList());
+			return Response.ok(affiliateDTOs).build();
 		} else {
-			return Response.status(204).build()	;
+			return Response.status(204).build();
 		}
 	}
 
 	@GET
 	@Path("{id}")
 	@Operation(summary = "Obtener un afiliado por ID", description = "Retorna un afiliado basado en el ID proporcionado.")
-	@APIResponses(value = {
-			@APIResponse(responseCode = "200", description = "Afiliado obtenido con éxito"),
-			@APIResponse(responseCode = "404", description = "Afiliado no encontrado")
-	})
+	@APIResponses(value = { @APIResponse(responseCode = "200", description = "Afiliado obtenido con éxito"),
+			@APIResponse(responseCode = "404", description = "Afiliado no encontrado") })
 	public Response getAffiliate(@PathParam("id") Long id) {
 		Affiliate affiliate = affiliateService.getAffiliateById(id);
 		if (affiliate == null) {
@@ -67,36 +55,34 @@ public class AffiliateResource {
 			return Response.ok(AffiliateMapper.entityToDto(affiliate)).build();
 		}
 	}
+
 	@POST
 	@Operation(summary = "Crear un afiliado", description = "Crea un nuevo afiliado.")
 	@APIResponse(responseCode = "200", description = "Afiliado creado con éxito")
 	@APIResponse(responseCode = "400", description = "Solicitud incorrecta, hay datos invalidos")
 	public Response createAffiliate(AffiliateDTO newAffiliateDTO) {
-		List<String> affiliateErrors = affiliateValidator.validateAffiliate(newAffiliateDTO);
-		if (affiliateErrors != null) {
-			return Response.status(Response.Status.BAD_REQUEST).entity(affiliateErrors.toString()).build();
-		}
 		try {
 			Affiliate affiliate = affiliateService.addAffiliate(AffiliateMapper.createAffiliateDto(newAffiliateDTO));
-			return Response.ok(AffiliateMapper.entityToDto(affiliate)).build();			
+			return Response.ok(AffiliateMapper.entityToDto(affiliate)).build();
+		} catch (IllegalArgumentException e) {
+			return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
 		} catch (Exception e) {
 			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
 		}
 	}
+
 	@PUT
 	@Operation(summary = "Actualiza un afiliado", description = "Actualiza un afiliado cuyo ID existe en el sistema, no se pueden ingresar datos vacios.")
 	@APIResponse(responseCode = "200", description = "Afiliado actualizado con éxito")
 	@APIResponse(responseCode = "400", description = "Solicitud incorrecta, hay datos invalidos")
 	@APIResponse(responseCode = "404", description = "Solicitud incorrecta, no existe el afiliado en el sistema con la ID ingresada")
 	public Response updateAffiliate(@PathParam("id") Long id, AffiliateDTO editAffiliateDTO) {
-		List<String> affiliateErrors = affiliateValidator.validateAffiliate(editAffiliateDTO);
-		if (affiliateErrors != null) {
-			return Response.status(Response.Status.BAD_REQUEST).entity(affiliateErrors.toString()).build();
-		}
 		try {
 			Affiliate updatedAffiliate = affiliateService.editAffiliate(id,
 					AffiliateMapper.updateAffiliateDto(editAffiliateDTO));
 			return Response.ok(AffiliateMapper.entityToDto(updatedAffiliate)).build();
+		} catch (IllegalArgumentException e) {
+			return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
 		} catch (Exception e) {
 			return Response.status(Status.NOT_FOUND).entity(e.getMessage()).build();
 		}
@@ -105,10 +91,8 @@ public class AffiliateResource {
 	@DELETE
 	@Path("{id}")
 	@Operation(summary = "Eliminar un afiliado", description = "Elimina un afiliado existente.")
-	@APIResponses(value = {
-			@APIResponse(responseCode = "200", description = "Afiliado eliminado con éxito"),
-			@APIResponse(responseCode = "404", description = "Afiliado no encontrado")
-	})
+	@APIResponses(value = { @APIResponse(responseCode = "200", description = "Afiliado eliminado con éxito"),
+			@APIResponse(responseCode = "404", description = "Afiliado no encontrado") })
 	public Response deleteAffiliateById(@PathParam("id") Long id) {
 		try {
 			Affiliate deletedAffiliate = affiliateService.deleteAffiliate(id);
--- BackendSofftek/obra-social-api/src/main/java/service/AffiliateService.java
+++ BackendSofftek/obra-social-api/src/main/java/service/AffiliateService.java
@@ -6,8 +6,11 @@ import jakarta.transaction.Transactional;
 import model.Affiliate;
 import repository.AffiliateRepository;
 import service.interfaces.IAffiliateService;
+import validator.AffiliateValidator;
 
 import java.util.List;
+
+import dto.mappers.AffiliateMapper;
 
 @ApplicationScoped
 @Transactional
@@ -15,6 +18,9 @@ public class AffiliateService implements IAffiliateService {
 
 	@Inject
 	private AffiliateRepository affiliateRepository;
+
+	@Inject
+	private AffiliateValidator affiliateValidator;
 
 	@Override
 	public List<Affiliate> getAllAffiliates() {
@@ -29,38 +35,44 @@ public class AffiliateService implements IAffiliateService {
 	@Override
 	public Affiliate addAffiliate(Affiliate newAffiliate) throws Exception {
 
-			if (affiliateRepository.findByDni(newAffiliate.getDni())!=null) throw new Exception("Ya existe un afiliado con el dni: "+newAffiliate.getDni());
-			if(affiliateRepository.findByHealthInsuranceCode(newAffiliate.getHealthInsuranceCode())!=null)throw new Exception("Ya existe un afiliado con el codigo de obra social: "+newAffiliate.getHealthInsuranceCode());
-			affiliateRepository.persist(newAffiliate);
-			return newAffiliate;
+		if (affiliateRepository.findByDni(newAffiliate.getDni()) != null)
+			throw new Exception("Ya existe un afiliado con el dni: " + newAffiliate.getDni());
+		if (affiliateRepository.findByHealthInsuranceCode(newAffiliate.getHealthInsuranceCode()) != null)
+			throw new Exception(
+					"Ya existe un afiliado con el codigo de obra social: " + newAffiliate.getHealthInsuranceCode());
+		List<String> existingErrors = affiliateValidator.validateAffiliate(AffiliateMapper.entityToDto(newAffiliate));
+		if (existingErrors != null)
+			throw new IllegalArgumentException(existingErrors.toString());
+		affiliateRepository.persist(newAffiliate);
+		return newAffiliate;
 
 	}
 
 	@Override
-	public Affiliate editAffiliate(Long id, Affiliate editedAffiliate) throws Exception{
+	public Affiliate editAffiliate(Long id, Affiliate editedAffiliate) throws Exception {
 		Affiliate existingAffiliate = affiliateRepository.findById(id);
-		if (existingAffiliate != null) {
-			existingAffiliate.setFirstName(editedAffiliate.getFirstName());
-			existingAffiliate.setLastName(editedAffiliate.getLastName());
-			existingAffiliate.setDni(editedAffiliate.getDni());
-			existingAffiliate.setHealthInsuranceCode(editedAffiliate.getHealthInsuranceCode());
-			affiliateRepository.persistAndFlush(existingAffiliate);
-			return existingAffiliate;
-		} else {
-			//return null;
+		if (existingAffiliate == null)
 			throw new Exception("El afiliado con id " + id + " no existe.");
-		}
+		List<String> existingErrors = affiliateValidator
+				.validateAffiliate(AffiliateMapper.entityToDto(editedAffiliate));
+		if (existingErrors != null)
+			throw new IllegalArgumentException(existingErrors.toString());
+
+		existingAffiliate.setFirstName(editedAffiliate.getFirstName());
+		existingAffiliate.setLastName(editedAffiliate.getLastName());
+		existingAffiliate.setDni(editedAffiliate.getDni());
+		existingAffiliate.setHealthInsuranceCode(editedAffiliate.getHealthInsuranceCode());
+		affiliateRepository.persistAndFlush(existingAffiliate);
+		return existingAffiliate;
 	}
 
 	@Override
 	public Affiliate deleteAffiliate(Long id) throws Exception {
 		Affiliate existingAffiliate = affiliateRepository.findById(id);
-		if (existingAffiliate != null) {
-			affiliateRepository.deleteById(id);
-			return existingAffiliate;
-		} else {
-//			return null;
+		if (existingAffiliate == null)
 			throw new Exception("El afiliado con id " + id + " no existe.");
-		}
+
+		affiliateRepository.deleteById(id);
+		return existingAffiliate;
 	}
 }
