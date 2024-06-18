bb33ada85e476e98157a160d20bf44eaed140eeb
Javier <kuznikjavier@gmail.com>
2024-06-18 13:55:35 -0300

    añadi ciertas cosas a mi rama

--- BackendSofftek/obra-social-api/src/main/java/service/interfaces/IAffiliateService.java
+++ BackendSofftek/obra-social-api/src/main/java/service/interfaces/IAffiliateService.java
@@ -9,7 +9,7 @@ import model.Affiliate;
 public interface IAffiliateService {
     public List<Affiliate> getAllAffiliates();
     public Affiliate getAffiliateById(Long id);
-    public Affiliate addAffiliate(Affiliate newAffiliate);
+    public Affiliate addAffiliate(Affiliate newAffiliate) throws Exception ;
     public Affiliate deleteAffiliate(Long id) throws Exception;
     public Affiliate editAffiliate(Long id, Affiliate newAffiliate)throws Exception;
 }
