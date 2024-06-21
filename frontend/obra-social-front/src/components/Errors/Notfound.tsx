a9429da174210e256737dcc56c8f17e4958d6483
joaquin muñoz <joaquinmatias99@gmail.com>
2024-06-21 20:39:45 -0300

    creacion de componentes iniciales para trabajar

---
+++ frontend/obra-social-front/src/components/Errors/Notfound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Definición del componente funcional
+const MyComponent: React.FC = () => {
+  return (
+    <>
+      <h1>NOT FOUND</h1>
+      
+    </>
+  );
+};
+
+export default MyComponent;
