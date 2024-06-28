d062ccd670bbc20c73a560ac0ad9918753c067ba
Javier <kuznikjavier@gmail.com>
2024-06-28 12:53:36 -0300

    resolví un bug con el traerFecha cercana

--- frontend/obra-social-front/src/funcionalities/Funcionalities.ts
+++ frontend/obra-social-front/src/funcionalities/Funcionalities.ts
@@ -1,37 +1,39 @@
 import { DayOfWeek } from "../redux/type";
 
 const dayOfWeekToIndex: { [key in DayOfWeek]: number } = {
-  [DayOfWeek.MONDAY]: 0,
-  [DayOfWeek.TUESDAY]: 1,
-  [DayOfWeek.WEDNESDAY]: 2,
-  [DayOfWeek.THURSDAY]: 3,
-  [DayOfWeek.FRIDAY]: 4,
-  [DayOfWeek.SATURDAY]: 5,
-  [DayOfWeek.SUNDAY]: 6,
+  [DayOfWeek.MONDAY]: 1,
+  [DayOfWeek.TUESDAY]: 2,
+  [DayOfWeek.WEDNESDAY]: 3,
+  [DayOfWeek.THURSDAY]: 4,
+  [DayOfWeek.FRIDAY]: 5,
+  [DayOfWeek.SATURDAY]: 6,
+  [DayOfWeek.SUNDAY]: 0, // Asegúrate de que domingo sea 0 para coincidir con getDay()
 };
 
 export const dayIndexToDayOfWeek: { [key: number]: DayOfWeek } = {
-  0: DayOfWeek.MONDAY,
-  1: DayOfWeek.TUESDAY,
-  2: DayOfWeek.WEDNESDAY,
-  3: DayOfWeek.THURSDAY,
-  4: DayOfWeek.FRIDAY,
-  5: DayOfWeek.SATURDAY,
-  6: DayOfWeek.SUNDAY,
+  1: DayOfWeek.MONDAY,
+  2: DayOfWeek.TUESDAY,
+  3: DayOfWeek.WEDNESDAY,
+  4: DayOfWeek.THURSDAY,
+  5: DayOfWeek.FRIDAY,
+  6: DayOfWeek.SATURDAY,
+  0: DayOfWeek.SUNDAY,
 };
 
 //esto trasnforma la fecha elegida en la mas proxima si se elige el domingo seria el 30 (?)
 export const getClosestDate = (day: DayOfWeek): string => {
   const dayIndex = dayOfWeekToIndex[day];
   const today = new Date();
-  const todayIndex = (today.getDay() + 1) % 7; // Ajustar índice de hoy de 1 a 7
+  const todayIndex = today.getDay(); // El índice de hoy sin ajustes
+
+  // Calcular los días hasta el próximo día de la semana deseado
   const daysUntilNext = (dayIndex - todayIndex + 7) % 7;
 
-  // Si daysUntilNext es 0, sumamos 7 para obtener la misma fecha de la próxima semana
+  // Si daysUntilNext es 0, eso significa que hoy es el día deseado, así que debemos sumar 7 días para obtener el mismo día la próxima semana
   const daysToAdd = daysUntilNext === 0 ? 7 : daysUntilNext;
 
   const nextDate = new Date(today);
-  nextDate.setDate(today.getDate() + (daysToAdd + 1));
+  nextDate.setDate(today.getDate() + daysToAdd);
 
   return nextDate.toISOString().split("T")[0]; // Formato YYYY-MM-DD
 };
