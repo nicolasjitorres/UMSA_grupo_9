64765218ee2a6f95f7dab43e9b2a4b9e7908771b
Javier <kuznikjavier@gmail.com>
2024-06-29 23:21:32 -0300

    pequeños cambios

--- frontend/obra-social-front/src/components/buttonToAdd/AddShiftButton.tsx
+++ frontend/obra-social-front/src/components/buttonToAdd/AddShiftButton.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 import BasicModal from "../../components/modal/Modal";
 
 const AddShiftButton: React.FC = () => {
   return (
     <>
       <h2>Nuevo Turno</h2>
-      <BasicModal name="Agregar" />
-      
+      <BasicModal name="Agregar" proveniencia="shift" />
     </>
   );
 };
