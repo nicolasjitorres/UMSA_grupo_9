8fa9c991f45f28b4bbf1a087177c057e2827bdc3
Javier <kuznikjavier@gmail.com>
2024-06-30 23:21:31 -0300

    corrección de un estética en row afiliado

--- frontend/obra-social-front/src/components/rows/RowAffiliate.tsx
+++ frontend/obra-social-front/src/components/rows/RowAffiliate.tsx
@@ -23,25 +23,39 @@ const RowAffiliate: React.FC<RowProps> = ({ affiliate }) => {
       <TableCell component="th" scope="row">
         {affiliate.firstName} {affiliate.lastName}
       </TableCell>
-      <TableCell align="right">{affiliate.dni}</TableCell>
-      <TableCell align="right">{affiliate.email}</TableCell>
-      <TableCell align="right">
-        {
+      <TableCell align="right" style={{ minWidth: 170 }}>
+        {affiliate.dni}
+      </TableCell>
+      <TableCell align="right" style={{ minWidth: 170 }}>
+        {affiliate.email}
+      </TableCell>
+      <TableCell align="center" style={{ minWidth: 170 }}>
+        <div
+          className="modal-button-container"
+          style={{
+            width: 170,
+            margin: "auto",
+            display: "flex",
+            justifyContent: "center",
+            gap: "10px", // Para separar los botones opcionalmente
+          }}
+        >
           <BasicModal
             name="Actualizar afiliado"
             affiliate={affiliate}
             proveniencia="affiliate" //esto lo ponemos para diferenciar a que form llamar en el modal
           />
-        }
+        </div>
+        <Button
+          variant="contained"
+          color="error"
+          startIcon={<DeleteIcon />}
+          onClick={() => handleDelete(affiliate.id)}
+          style={{ marginLeft: "10px" }} // Opcional, para separar los botones
+        >
+          Borrar
+        </Button>
       </TableCell>
-      <Button
-        variant="contained"
-        color="error"
-        startIcon={<DeleteIcon />}
-        onClick={() => handleDelete(affiliate.id)}
-      >
-        Borrar
-      </Button>
     </TableRow>
   );
 };
